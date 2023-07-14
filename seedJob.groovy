96707ada62e03dc0fcbb9b056a995316060f0767
Rahul-Palande11 <131943341+Rahul-Palande11@users.noreply.github.com>
2023-07-14 15:03:03 +0530

    Create seedJob.groovy

---
+++ seedJob.groovy
@@ -0,0 +1,18 @@
+folder('JobDsl') {
+    freeStyleJob('JobDsl/folder') {
+        scm {
+            git {
+                branch('main')
+                remote {
+                    url('https://github.com/Rahul-Palande11/Assignment7.git')
+                }
+            }
+        }
+        steps {
+            dsl {
+                external('JobDsl/*/folders.jenkins')
+                external('JobDsl/**/*_job.jenkins')
+            }
+        }
+    }
+}
